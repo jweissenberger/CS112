0cd05cad1ccc58424e4c2960ac8616e1d49724db
John Weissenberger <johnaweissenberger@gmail.com>
2017-11-08 09:54:07 -0500

    Finished pt 0

--- RecursionLab/Part0/Recur_Fun.cpp
+++ RecursionLab/Part0/Recur_Fun.cpp
@@ -11,19 +11,24 @@ using namespace std;
 string Recur_Fun::str_rev(string inStr) {
     string temp;
 
-    //base case
+    //base case, once the string is reduced to one letter
     if (inStr.length() == 1){
         return inStr;
     }
+
+    //recursive case, letters are reversed one by one until the input string is reduced to 1
     else{
         return str_rev(inStr.substr(1, inStr.length())) + inStr.at(0);
     }
 
 }
 
+// recursively checks each value in a vector so find which value is the largest
 //starts with the last value of the vector and then iterates down
 int Recur_Fun::list_max(vector<int> inList, int i, int & maxVal) {
 
+    // recursive case, if the value of the vector at i is larger than the previous max value
+    // it is replaced and then then the function is called again to check the next value
     if (i != inList.size()){
         if (inList.at(i) > maxVal){
             maxVal = inList.at(i);
@@ -31,19 +36,18 @@ int Recur_Fun::list_max(vector<int> inList, int i, int & maxVal) {
         list_max(inList, ++i, maxVal);
     }
 
-        //base case
+    //base case
     else{
         return 0;
     }
 
-    //recursive case, iterates down through the vector
-
 }
 
 
-//if it outputs a value greater than 0 then it is true
-// checks to see if the user input is a pallindrome, checks to see that the outer most are the same and then works its
+
+// checks to see if the user input is a palindrome, checks to see that the outer most are the same and then works its
 // way in, checking they are all the same
+//if it outputs a value greater than 0 then it is true, 0 if it is not true
 bool Recur_Fun::palin(string usrIn) {
     int len = usrIn.length();
 
@@ -63,19 +67,24 @@ bool Recur_Fun::palin(string usrIn) {
     }
 }
 
+// checks to each substring of the larger string of the same length of the smaller string to see if it is
+// the same as the substring
 //outputs a non zero value if the substring exists, outputs 0 if it does not exist
 int Recur_Fun::find_sub(string big, string small, int &i) {
     int bigLen = big.size();
     int smallLen = small.size();
 
+    // the substring is found in the string
     if (small == big.substr(i, smallLen)){
         return 1;
     }
 
-    if (i < bigLen) {
+    //recursive case
+    if (i <= (bigLen-1)) {
         find_sub(big, small, ++i);
     }
 
+    // if the string as been iterated through and, there has not been a match, output 0
     if (i == bigLen){
         return 0;
     }
--- RecursionLab/Part0/main.cpp
+++ RecursionLab/Part0/main.cpp
@@ -1,3 +1,6 @@
+// Jack Weissenberger Recursion Lab part 0
+// This program has a class with 4 recursive methods that are accessed by user input
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,50 +10,85 @@ using namespace std;
 
 int main() {
 
+    //initialize variables
     Recur_Fun recur;
     char inCmd;
     string str1;
     int vecInput;
     vector<int> maxValVec;
+    string big, small;
+    int maxV = 0;
+    int i = 0;
 
     cout << "Enter a command: " << endl;
     cin >> inCmd;
 
+    // keep running commands as long as the user does not enter E
     while (inCmd != 'E'){
         switch (inCmd){
+
+            //if the user inputs R, the program outputs the string backwards
             case 'R':
+            case 'r':
                 cout << "Input a string to be reversed: ";
                 cin >> str1;
                 cout << endl << recur.str_rev(str1);
                 break;
 
+            // if the user inputs M, the user inputs values into a vector and the program
+            // outputs the largest value
             case 'M':
+            case 'm':
                 cout << "Enter values to be inputted into the vector: ";
                 cin >> vecInput;
+
+                //keeps getting input until the user inputs a negative value
                 while (vecInput >= 0){
                     maxValVec.push_back(vecInput);
                     cin >> vecInput;
                 }
-                int maxV = 0;
+
                 recur.list_max(maxValVec, 0, maxV);
                 cout<< endl << "Maximum value is: " << maxV;
                 break;
-                
+
+            // if the user inputs P, the the program checks if the inputted string is a palindrome
+            case 'P':
+            case 'p':
+                cout << "Input a string to check if it is a palindrome:  ";
+                cin >> str1;
+                if (recur.palin(str1) > 0){
+                    cout << endl << true << " It is a palindrome";
+                } else{
+                    cout << endl << false << " It is not a palindrome";
+                }
+                break;
+
+            // if the user inputs F, the program checks to see if one of the uer inputtted strings
+            // is a substring of the other
+            case 'F':
+            case 'f':
+                cout << "Input the larger string: ";
+                cin >> big;
+                cout << endl << "Input the substring: ";
+                cin >> small;
+
+                if (recur.find_sub(big, small, i) > 0){
+                    cout << endl << true << " It is a substring";
+                } else{
+                    cout << endl << false << " It is not a substring";
+                }
+                break;
+
+
+            // if the user enters an unknown command, the loop is run again
+            default:
+                cout << "Bad Command" << endl;
+                break;
         }
-
+        cout << endl << "Command: " << endl;
+        cin >> inCmd;
     }
-
-    //to run each of the recursive functions you need to create a switch statement
-
-    string str = "whywontthisworkthen";
-    string str2 = "ywontth";
-
-    cout<< recur.str_rev(str);
-
-
-    cout << endl << str.substr(0, str2.length());
-    int i = 0;
-    cout << endl << recur.find_sub(str, str2, i) << endl;
 
     return 0;
 }
