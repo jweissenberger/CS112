a24fff6b5a06d6f894ed1d782aa75c75e4e3c0e3
John Weissenberger <johnaweissenberger@gmail.com>
2017-11-08 15:50:57 -0500

    Finish Part 2

--- RecursionLab/Part2/main.cpp
+++ RecursionLab/Part2/main.cpp
@@ -1,58 +1,82 @@
+//Jack Weissenberger Recursive lab Part 2
+// Merge two vectors using recursion, each should be in numerical order
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-void q_merge (vector <int> &a, vector <int> &b, vector <int> &c);
+void q_merge (vector <int> &vec1, vector <int> &vec2, vector <int> &vecNew);
 
-void q_merge (vector <int> &a, vector <int> &b, vector <int> &c){
-    if(a.size() == 0 && b.size() == 0){
+void q_merge (vector <int> &vec1, vector <int> &vec2, vector <int> &vecNew){
+
+    //base case, when both vectors have no more values
+    if(vec1.empty() && vec2.empty()){
         return;
     }
-    else if (b.size() == 0){
-        c.push_back((int)a.at(0));
-        a.erase(a.begin());
+
+    //if vec2 has no more values fill the merge vector with values from vec1
+    else if (vec2.size() == 0){
+        vecNew.push_back((int)vec1.at(0));
+        vec1.erase(vec1.begin());
     }
-    else if(a.size()== 0){
-        c.push_back((int)b.at(0));
-        b.erase(b.begin());
+
+    //if vector 2 is larger, input vector 1
+    else if(vec1.at(0)<vec2.at(0)){
+        vecNew.push_back((int)vec1.at(0));
+        vec1.erase(vec1.begin());
     }
-    else if(a.at(0)<b.at(0)){
-        c.push_back((int)a.at(0));
-        a.erase(a.begin());
+
+    //if vec1 has no more values, fill the merge vector with values from vec2
+    else if(vec1.size()== 0){
+        vecNew.push_back((int)vec2.at(0));
+        vec2.erase(vec2.begin());
     }
+
+    //this is the case that vector1 is larger, so the value from vector2 is inputted
     else{
-        c.push_back((int)b.at(0));
-        b.erase(b.begin());
+        vecNew.push_back((int)vec2.at(0));
+        vec2.erase(vec2.begin());
     }
-    q_merge(a, b, c);
+    q_merge(vec1, vec2, vecNew);
 
     return;
 }
 
 int main() {
-    vector <int> vect_A, vect_B, vect_C;
-    unsigned int size_A, size_B, inInt, i;
+
+    vector <int> vecA;
+    vector <int> vecB;
+    vector <int> vMerge;
+    unsigned int sizeA, sizeB, userIn, i;
 
 
+    cout << "Input the size of A" << endl;
+    cin >> sizeA;
 
-    cin >> size_A;
+    cout << "Input the size of B" << endl;
+    cin >> sizeB;
 
-    cin >> size_B;
-    for ( i=0; i<size_A;++i) {
-        cin >> inInt;
-        vect_A.push_back((int)inInt);
+    //fill the values of vector A
+    cout << "Input values into A" << endl;
+    for ( i=0; i<sizeA;++i) {
+        cin >> userIn;
+        vecA.push_back((int)userIn);
     }
-    for (i=0; i<size_B;++i) {
-        cin >> inInt;
-        vect_B.push_back((int)inInt);
-    }
-    q_merge (vect_A, vect_B, vect_C);
 
-    // Output the merged vector
-    for (i=0; i<vect_C.size(); ++i) {
-        cout << vect_C.at(i) << " ";
+    cout << "Input values into B" << endl;
+    //Fill the values of vectorB
+    for (i=0; i<sizeB;++i) {
+        cin >> userIn;
+        vecB.push_back((int)userIn);
+    }
+    q_merge (vecA, vecB, vMerge);
+
+    // print the newly merged vector
+    for (i=0; i<vMerge.size(); ++i) {
+        cout << vMerge.at(i) << " ";
     }
     cout << endl;
 
     return 0;
 }
+
