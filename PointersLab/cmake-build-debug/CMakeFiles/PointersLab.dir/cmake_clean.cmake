eafd03b6940d1ce4b7587aabc0c15f53818068d1
John Weissenberger <johnaweissenberger@gmail.com>
2017-11-12 15:28:36 -0500

    First Parts of pointers lab

---
+++ PointersLab/cmake-build-debug/CMakeFiles/PointersLab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointersLab.dir/main.cpp.obj"
+  "PointersLab.pdb"
+  "PointersLab.exe"
+  "PointersLab.exe.manifest"
+  "libPointersLab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointersLab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PointersLab/main.cpp
@@ -0,0 +1,76 @@
+// Jack Weissenberger, recursion lab
+
+#include <iostream>
+using namespace std;
+
+void printVariableInformation();
+
+void printVariableInformation(){
+    static double* doubValptr, doubVal = 23.7;
+    static int* intValptr, intVal = 5;
+    static bool* boolValptr, boolVal = false;
+
+    cout << "Double value: " << doubVal << ", Double value pointer: " << doubValptr << endl;
+    cout << "Boolean value: " << boolVal << ", Boolean value pointer: " << boolValptr << endl;
+    cout << "Integer value: " << intVal << ", Integer value pointer: " << intValptr << endl;
+
+    cout << sizeof(doubVal) << endl;
+    cout << sizeof(boolVal) << endl;
+    cout << sizeof(intVal) << endl;
+
+
+}
+
+void printArrayInformation();
+
+void printArrayInformation() {
+    int arrayOfIntegers[10];
+    int j = 20;
+    for (int i = 0; i < 10; ++i) {
+        arrayOfIntegers[i] = j;
+        j += 2;
+
+    }
+
+    cout << arrayOfIntegers        << endl;
+
+    cout << &(arrayOfIntegers[0])  << endl;
+    cout << arrayOfIntegers + 0    << endl;
+    cout << arrayOfIntegers[0]     << endl;
+    cout << *(arrayOfIntegers + 0) << endl;
+
+    cout << &(arrayOfIntegers[1])  << endl;
+    cout << arrayOfIntegers + 1    << endl;
+    cout << arrayOfIntegers[1]     << endl;
+    cout << *(arrayOfIntegers + 1) << endl;
+    cout << (*arrayOfIntegers) + 1 << endl;
+
+    cout << &(arrayOfIntegers[10]) << endl;
+    cout << arrayOfIntegers + 10   << endl;
+    //cout << arrayOfIntegers[10]    << endl;
+
+}
+
+void foo1(int xval){
+    int x = xval;
+    cout << x << endl << &x << endl;
+}
+
+void foo2(int dummy){
+    int y = dummy;
+    cout << y << endl << &y << endl;
+
+}
+
+
+
+
+int main() {
+
+    //printVariableInformation();
+    //printArrayInformation();
+    //foo1(10);
+    //foo2(10);
+
+    return 0;
+}
